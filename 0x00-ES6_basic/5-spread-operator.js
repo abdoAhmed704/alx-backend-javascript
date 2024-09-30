4c1ec1855900abde44f4c1e0ed5ea51d4bbad08a
= <abdoahmedsayed704@gmail.com>
2024-10-01 01:53:58 +0300

    push

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,13 +1,3 @@
-// let array1 = [2, 3];
-// let array2 = [6, 8];
-// let arr = [...array1, ...array2, ..."AA".split("")];
-// console.log(arr);
-
-// function concatArrays(array1, array2, string) {
-    
-// }
-// console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
-
 export default function concatArrays(array1, array2, string) {
     return [...array1, ...array2, ...string.split("")];
 }
