bd627ccf43ce6f860d667ad6a96bf201d98c70dd
= <joten1000@gmail.com>
2024-11-26 16:58:01 +0200

    Done

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,15 +1,3 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
-// Arrow function
-displayMessage = (massage) => console.log(massage)
+const displayMessage = (massage) => console.log(massage)
 
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-// Default module export.
 module.exports = displayMessage;
