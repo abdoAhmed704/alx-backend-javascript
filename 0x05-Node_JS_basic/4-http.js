7e98a30888303f3d4cb3342b69b36d324ae695ca
= <joten1000@gmail.com>
2024-11-27 14:36:29 +0200

    Done.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,19 +2,16 @@ const http = require('http');
 
 const PORT = 1245;
 const HOST = 'localhost';
-const app = http.createServer();
 
-app.on('request', (_, res) => {
-  const responseText = 'Hello Holberton School!';
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length);
-  res.statusCode = 200;
-  res.write(Buffer.from(responseText));
+  // Send the response body
+  res.end('Hello Holberton School!');
 });
 
-app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+// Start the server on a specified port
+server.listen(PORT, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
 });
-
-module.exports = app;
